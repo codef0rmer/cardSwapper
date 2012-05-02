9fc74b047925a09dec329940dbb733bf2b1fd325
codef0rmer <amit.2006.it@gmail.com>
2012-05-02 18:01:42 +0530

    Moved rows and cols properties in Collection and have a separate model for each image.

--- js/script.js
+++ js/script.js
@@ -4,21 +4,21 @@
  * 	http://amitgharat.wordpress.com/
  * 
  */
-var csCollection = Backbone.Collection.extend({
-	
-});
+var csModel = Backbone.Model.extend({});
 
-var csModel = Backbone.Model.extend({
-	defaults : {
-		arrImage 	: ['cat.svg', 'camel.svg', 'crow.svg', 'dog.svg', 'seal.svg'], 
-		rows 		: 2, 
-		cols		: 5
-	}, 
-  
-  pickRandomCard : function () {
-    return this.get('arrImage')[ Math.random() * this.get('arrImage').length | 0 ];
-  }	
+var csCollection = Backbone.Collection.extend({
+	model : csModel, 
+	
+  initialize : function () {
+    this.rows = 2;
+    this.cols = 5;
+  }, 
+	
+	pickRandomCard : function () {
+		return (this.models)[ Math.random() * this.models.length | 0 ].get('pic');
+	}
 });
+var csc = new csCollection([{pic : 'cat.svg'}, {pic : 'camel.svg'}, {pic : 'crow.svg'}, {pic : 'dog.svg'}, {pic : 'seal.svg'}]);
 
 var csView = Backbone.View.extend({
 	el : $('body'),   
@@ -29,20 +29,20 @@ var csView = Backbone.View.extend({
   
   initialize : function () {
     this.template = $('#cellTemplate').children();
-    this.arrCacheImage = []; 
     this.arrLocacheImage = [];
     this.click = 1;
-	},
+  },
   
   render : function () {
     var index = 0;
     this.arrCacheImage = [];
+        
     $(this.el).find('#cards').empty();
-    for (var row = 1; row <= this.model.get('rows'); row++) {
+    for (var row = 1; row <= csc.rows; row++) {
       var $tableRow = $('<div />').addClass('row');
-      for (var col = 1; col <= this.model.get('cols'); col++) {
+      for (var col = 1; col <= csc.cols; col++) {
         if (this.arrLocacheImage.length === 0) {
-          var image = this.model.pickRandomCard();          
+          var image = csc.pickRandomCard();          
         } else {
           var image = this.arrLocacheImage[index];
         }
@@ -77,18 +77,19 @@ var csRouter = Backbone.Router.extend({
   }, 
   
   initialize : function () {
-    this.view = new csView({ model : new csModel() });
+    this.view = new csView({});
     locache.flush();
     window.location.href = window.location.href.substring(0, window.location.href.indexOf('#')) + '#1';
   }, 
   
   reload : function (t) {
+    this.view.click = t;
     this.view.reload(t);
   }
 });
 
 /* Boot the Application */
 $(function () {
-    new csRouter();
-    Backbone.history.start();
+  new csRouter();
+  Backbone.history.start();
 });
