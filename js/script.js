2f3eb066ff86356e50729c3711cb9e62a9c5b3a2
codef0rmer <amit.2006.it@gmail.com>
2012-05-02 18:04:59 +0530

    Indented the code with 2 spaces.

--- js/script.js
+++ js/script.js
@@ -1,42 +1,42 @@
 /*	Author:
- *	
- *	Amit Gharat a.k.a. codef0rmer
- * 	http://amitgharat.wordpress.com/
- * 
- */
+*	
+*	Amit Gharat a.k.a. codef0rmer
+* 	http://amitgharat.wordpress.com/
+* 
+*/
 var csModel = Backbone.Model.extend({});
 
 var csCollection = Backbone.Collection.extend({
-	model : csModel, 
-	
+  model : csModel, 
+
   initialize : function () {
     this.rows = 2;
     this.cols = 5;
   }, 
-	
-	pickRandomCard : function () {
-		return (this.models)[ Math.random() * this.models.length | 0 ].get('pic');
-	}
+
+  pickRandomCard : function () {
+    return (this.models)[ Math.random() * this.models.length | 0 ].get('pic');
+  }
 });
 var csc = new csCollection([{pic : 'cat.svg'}, {pic : 'camel.svg'}, {pic : 'crow.svg'}, {pic : 'dog.svg'}, {pic : 'seal.svg'}]);
 
 var csView = Backbone.View.extend({
-	el : $('body'),   
-  
+  el : $('body'),   
+
   events : {
     "click #reload" : "reloadClick"
   }, 
-  
+
   initialize : function () {
     this.template = $('#cellTemplate').children();
     this.arrLocacheImage = [];
     this.click = 1;
   },
-  
+
   render : function () {
     var index = 0;
     this.arrCacheImage = [];
-        
+
     $(this.el).find('#cards').empty();
     for (var row = 1; row <= csc.rows; row++) {
       var $tableRow = $('<div />').addClass('row');
@@ -53,12 +53,12 @@ var csView = Backbone.View.extend({
       $(this.el).find('#cards').append($tableRow);
     }
   }, 
-  
+
   reloadClick : function () {
     this.click++;
     window.location.href = window.location.href.substring(0, window.location.href.indexOf('#')) + '#' + this.click;
   },
-  
+
   reload : function (locacheKey) {
     if (locache.get(locacheKey) === null) {
       this.arrLocacheImage = [];
@@ -75,13 +75,13 @@ var csRouter = Backbone.Router.extend({
   routes : {
     ':timestamp' : 'reload'
   }, 
-  
+
   initialize : function () {
     this.view = new csView({});
     locache.flush();
     window.location.href = window.location.href.substring(0, window.location.href.indexOf('#')) + '#1';
   }, 
-  
+
   reload : function (t) {
     this.view.click = t;
     this.view.reload(t);
